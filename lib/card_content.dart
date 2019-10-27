871c61706c33a06d0f8feb7c4dd8da16691f5248
Gurupratap <gurupratap.matharu@gmail.com>
2019-10-27 15:02:12 -0300

    card content moved to a new dart file

---
+++ lib/card_content.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+const labelStyle = TextStyle(
+  color: Color(0xFF8D8E98),
+  fontSize: 18.0,
+);
+
+class CardContent extends StatelessWidget {
+  CardContent({this.icon, this.label});
+
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: labelStyle,
+        )
+      ],
+    );
+  }
+}
