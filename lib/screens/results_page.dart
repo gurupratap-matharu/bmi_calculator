39fd7f99308ee4eba7322eca26ab23e48b0df1f6
Gurupratap <gurupratap.matharu@gmail.com>
2019-10-29 17:01:44 -0300

    results page updated with active bmi results

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,9 +1,18 @@
-import 'package:bmi_calculator/reusable_card.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
 import 'package:flutter/material.dart';
-import 'constants.dart';
-import 'bottom_button.dart';
+import '../constants.dart';
+import '../components/bottom_button.dart';
 
 class ResultsPage extends StatelessWidget {
+  ResultsPage(
+      {@required this.bmiResult,
+      @required this.bmiText,
+      @required this.bmiInterpretation});
+
+  final String bmiResult;
+  final String bmiText;
+  final String bmiInterpretation;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,15 +36,15 @@ class ResultsPage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Text(
-                    'Normal',
+                    bmiText.toUpperCase(),
                     style: kResultsBodyTextStyle,
                   ),
                   Text(
-                    '21.5',
+                    bmiResult,
                     style: kResultsTitleTextStyle,
                   ),
                   Text(
-                    'Your BMI is low and you should eat more!',
+                    bmiInterpretation,
                     style: kResultsBodyTextStyle,
                     textAlign: TextAlign.center,
                   ),
